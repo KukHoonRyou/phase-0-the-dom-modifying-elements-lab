6fc9eb0d715369a486f56b7e23807a266e829480
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-10 00:14:08 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//document.querySelector("main#main").remove();
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+
+newHeader.textContent = "Michael is the champion";
+//newHeader.innerHTML = "Michael is the champion";
+//newHeader.innerText = "Michael is the champion";
+newHeader.className = "victory";
 // Write your code here!
