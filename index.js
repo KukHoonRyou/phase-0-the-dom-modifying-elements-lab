36618c9909795ccb3fdb2e158596e491176d4e46
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-22 12:53:15 -0500

    teacher's solution

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+/*
 //document.querySelector("main#main").remove();
 main.remove()
 const newHeader = document.createElement("h1");
@@ -8,3 +10,26 @@ newHeader.textContent = "Michael is the champion";
 //newHeader.innerText = "Michael is the champion";
 newHeader.className = "victory";
 // Write your code here!
+
+*/
+
+//teacher's solution
+let main = document.getElementById('main')
+// # is for ids, . is for classes
+main = document.querySelector('#main')
+main.remove()
+
+for(let i = 0; i < 10; i++){
+
+}
+
+const newHeader = document.createElement('h1')
+    newHeader.id = 'victory'
+    newHeader.textContent = "yourName is the champion"
+    body.append(newHeader)
+
+let htmlcollection = document.getElementsByTagName('h1') //get all tags
+
+Array.from(document.getElementsByTagName('h1')).forEach((el) => el.remove())// turn into Array
+//getElementsByTagname and getElementsByClassName isll return array
+const body = document.getElementsByTagName('body')[0]
